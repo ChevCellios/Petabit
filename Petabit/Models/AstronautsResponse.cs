61c4cc01e8f60fa4e9662a36f7093fcfd8c026da
ChevCellios <midom.croatia@gmail.com>
2025-08-07 21:27:59 +0200

    Final Commit GitHub

--- Petabit/Models/AstronautsResponse.cs
+++ Petabit/Models/AstronautsResponse.cs
@@ -3,14 +3,14 @@
     public class AstronautsResponse
     {
         public string Message { get; set; }
-        public int Number { get; set; }
+        public int? Number { get; set; }
         public List<Astronaut> People { get; set; }
     }
 
     public class Astronaut
     {
-        public string Name { get; set; }
-        public string Craft { get; set; }
+        public string? Name { get; set; }
+        public string? Craft { get; set; }
     }
 }
 
--- Petabit/Models/IssLocationResponse.cs
+++ Petabit/Models/IssLocationResponse.cs
@@ -2,16 +2,16 @@
 {
     public class IssLocationResponse
     {
-        public string Message { get; set; }
-        public string Timestamp { get; set; }
+        public string? Message { get; set; }
+        public string? Timestamp { get; set; }
 
-        public IssPosition IssPosition { get; set; }
+        public IssPosition? IssPosition { get; set; }
     }
 
     public class IssPosition
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
 
     }
 }
